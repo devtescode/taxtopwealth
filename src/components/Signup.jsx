9cf5aa16eb8399ad1540e5412639c246e7303b17
agboola teslim <teslimagboola09@gmail.com>
2024-03-26 11:42:56 +0100

    addsignup

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -3,8 +3,33 @@ import React from 'react'
 const Signup = () => {
     return (
         <>
-            {/* <input type="text" /> */}
-            <h1>Signup</h1>
+            <div class="containersignup">
+                <div class="card">
+                    <a class="login">Register</a>
+                    <div class="inputBox">
+                        <input type="text" required="required"/>
+                            <span class="user">Username</span>
+                    </div>
+
+                    <div class="inputBox">
+                        <input type="password" required="required"/>
+                            <span>Password</span>
+                    </div>
+                    
+                    <div class="inputBox">
+                        <input type="password" required="required"/>
+                            <span>Refeeral</span>
+                    </div>
+
+                    <div class="inputBox">
+                        <input type="password" required="required"/>
+                            <span>Email</span>
+                    </div>
+
+                    <button class="enter">Sign Up</button>
+
+                </div>
+            </div>
         </>
     )
 }
