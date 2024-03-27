e4232c5cd11b54e0856852a56ce31f48c64570c5
agboola teslim <teslimagboola09@gmail.com>
2024-03-27 12:06:10 +0100

    addup

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,35 +1,67 @@
 import React from 'react'
-
+import { useFormik } from 'formik'
+import * as Yup from "yup"
+import axios from 'axios'
 const Signup = () => {
+    const formik = useFormik({
+        initialValues: {
+            username: ""
+        },
+        validationSchema: Yup.object({
+            username: Yup.string()
+                .required('Required'),
+        }),
+        onSubmit: values => {
+            axios.post("http://localhost:3600/facebook/Signup", { Username: values.username })
+                .then((response) => {
+                    alert("add")
+                })
+        }
+    })
     return (
         <>
             <div class="containersignup">
                 <div class="card">
                     <a class="login">Register</a>
                     <div class="inputBox">
-                        <input type="text" required="required"/>
-                            <span class="user">Username</span>
+                        <input
+                            type="text"
+                            required="required"
+                            className={`form-control ${(formik.values.username && !formik.errors.username) ||
+                                (formik.touched.username && formik.values.username && formik.errors.username && formik.touched.username && formik.values.username)
+                                ? 'is-valid'
+                                : formik.values.username || formik.touched.username
+                                    ? 'is-invalid'
+                                    : ''
+                                }`}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            name="username"
+                            value={formik.values.username}
+                            placeholder="Username"
+                        />
+                        <span class="user">Username</span>
                     </div>
 
                     <div class="inputBox">
-                        <input type="password" required="required"/>
-                            <span>Email</span>
+                        <input type="text" required="required" />
+                        <span>Email</span>
                     </div>
 
                     <div class="inputBox">
-                        <input type="password" required="required"/>
-                            <span>Phone Number</span>
-                    </div>
-                    
-                    <div class="inputBox">
-                        <input type="password" required="required"/>
-                            <span>Refeeral</span>
+                        <input type="number" required="required" />
+                        <span>Phone Number</span>
                     </div>
 
-                   
                     <div class="inputBox">
-                        <input type="password" required="required"/>
-                            <span>Password</span>
+                        <input type="text" required="required" />
+                        <span>Refeeral</span>
+                    </div>
+
+
+                    <div class="inputBox">
+                        <input type="password" required="required" />
+                        <span>Password</span>
                     </div>
                     <button class="enter">Sign Up</button>
                 </div>
